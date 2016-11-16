b5f6e1842b3273170abcef1d6d556a1059865928
Laimonas Danyla <laimonas.danyla@gmail.com>
2016-11-16 20:54:12 +0200

    refactor: add map and listview

---
+++ js/app.js
@@ -0,0 +1,14 @@
+//Create map
+var map;
+//Initialize function of the map
+function initMap() {
+       var uluru = {lat: -25.363, lng: 131.044};
+       map = new google.maps.Map(document.getElementById('map'), {
+         zoom: 4,
+         center: uluru
+       });
+       var marker = new google.maps.Marker({
+         position: uluru,
+         map: map
+       });
+     }
